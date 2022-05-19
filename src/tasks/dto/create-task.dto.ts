42d43f323df6792b6e62baa062401ce20e47d6f2
ahdiehemadi <ahdieh_e@yahoo.com>
2022-05-19 12:40:35 -0400

    Create IsNotEmpty Validation Pipe

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,9 @@
+import { IsNotEmpty } from 'class-validator';
+
 export class CreaeTaskDto {
   // we use the parameters in Create task handler in the task controller
+  @IsNotEmpty()
   title: string;
+  @IsNotEmpty()
   description: string;
 }
